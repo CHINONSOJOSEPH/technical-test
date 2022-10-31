028d90a173e50aa7ecfce19e9b22ea5c385d4608
CHINONSOJOSEPH <CHINONSOJOSEPH8182@GMAIL.COM>
2022-10-31 09:49:35 +0100

    https://github.com/CHINONSOJOSEPH/technical-test

---
+++ technical test.js
@@ -0,0 +1,15 @@
+<script>
+
+function nth_rarestitem (arr){//assignment question
+
+count list,nth_rarestitem
+let nth= item[0]
+const counted=item.reduce((acc,item))
+letn =[5,4,5,4,5,4,4,5,3,3,3,2,2,1,5];
+if (!acc[item]){acc[item]=1}
+else{acc[item]++} 
+if(acc[item]<acc[nth_rarestitem])
+{nth_rarestitem=item}
+return acc
+console.log(nth_rarestitem)
+</script>
